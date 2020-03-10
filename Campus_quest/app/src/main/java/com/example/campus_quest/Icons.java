3e747fe14a40a8fad7441da337cf1fc374c9d37d
basel-shehabi <baselshehabi1@gmail.com>
2020-03-10 16:13:24 +0000

    Added files

---
+++ Campus_quest/app/src/main/java/com/example/campus_quest/Icons.java
@@ -0,0 +1,14 @@
+package com.example.campus_quest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Icons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+    }
+}
